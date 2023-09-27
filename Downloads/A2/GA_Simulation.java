9e4f480f8ad16d165e972dd44b84eb5690ca2657
SanjanaYasna <sanyasna517@gmail.com>
2023-09-26 20:08:56 -0400

    not quite sure

---
+++ Downloads/A2/GA_Simulation.java
@@ -0,0 +1,96 @@
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+public class GA_Simulation {
+   /* int n = 100;
+    int k = 15;
+    int r = 100;
+    int c_0 = 8;
+    int c_max = 20;
+    double m = 0.01;*/
+    int n;
+    int k;
+    int r;
+    int c_0;
+    int c_max;
+    double m;
+
+  //  public ArrayList<ArrayList<Character>> members;
+    public ArrayList<Individual> members;
+    public ArrayList<Individual> generationParents;
+    public List<Integer> randomparents;
+
+    private GA_Simulation(int n, int k, int r, int c_0, int c_max, double m){
+        this.n = n;
+        this.k = k;
+        this.r = r;
+        this.c_0 = c_0;
+        this.c_max = c_max;
+        this.m = m;
+    }
+
+    private void init(){
+        members = new ArrayList<Individual>(n);
+        for (int i = 0; i<this.n; i++){
+            Individual individual = new Individual(8);
+            members.add(individual);
+        }
+        System.out.println(members);
+    }
+
+    public void rankPopulation(ArrayList<Individual> pop) {
+      // sort population by fitness
+      Comparator<Individual> ranker = new Comparator<>() {
+        // this order will sort higher scores at the front
+        public int compare(Individual c1, Individual c2) {
+          return (int)Math.signum(c2.getFitness() -c1.getFitness());
+        }
+      };
+      pop.sort(ranker); 
+    }
+
+    public void describeGeneration(){
+      System.out.println("Fitness of first member is: " + members.get(1).getFitness());
+      System.out.println("Fitness of the 15th member is: " + members.get(14).getFitness());
+      System.out.println("Fitness of the last member is: " +members.get(99).getFitness());
+    }
+
+
+   /*Collections.sort(members, new Comparator<ArrayList<Character>>() {
+      @Override
+      public int compare(ArrayList<String> one, ArrayList<String> two) {
+        return one.get(1).compareTo(two.get(1));
+    }
+});
+    }*/
+
+    public void evolve(){
+      generationParents = new ArrayList<Individual>(members.subList(0, k));
+     // System.out.println(generationParents);
+     // System.out.println(generationParents.size());
+     ArrayList<Individual> newMembers = new ArrayList<Individual>(n); //to eventually set equal to arraylist of members
+      for (int i = 0; i < n; i++){ //have 2 individuals breed enough times to create n offspring for next generation
+        List<Integer> pairing =  ThreadLocalRandom.current().ints(0, 15).limit(2).boxed().toList();
+        System.out.println(pairing);
+       //create filler individual arraylists that get from teh members arraylist
+        ArrayList<Individual> indv1 = new ArrayList<Individual>();
+        ArrayList<Individual> indv2 = new ArrayList<Individual>();
+        indv1.add(generationParents.get(pairing.get(0)));
+        indv2.add(generationParents.get(pairing.get(1)));
+        Individual offspring = new Individual(indv1, indv2, m, c_max);
+        newMembers.add(offspring);
+      }
+      members = newMembers;
+      //members now ponts to this new set of members/offspring 
+    }
+
+
+
+    public static void main(String[] args){
+        GA_Simulation simulation = new GA_Simulation(100, 15, 100, 8, 20, 0.01);
+        simulation.init(); //yep, makes members
+        simulation.rankPopulation(simulation.members);
+        //System.out.println(simulation.members); //yep, sticks to 100 of those, and it seems to be pretty correct!
+        simulation.describeGeneration(); //mumbers descend, so looks good
+        simulation.evolve();
+    }
+}
---
+++ Downloads/A2/Individual.java
@@ -0,0 +1,191 @@
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Individual{
+    public  ArrayList<Character> chromosome = new ArrayList<Character>(); //individual's chromosomes
+    //population's chromosomes, 100 intitially
+    public ArrayList<ArrayList<Character>> memberChromosomes = new ArrayList<ArrayList<Character>>(100); 
+    public  ArrayList<Character> offspringChromosome;
+    public ArrayList<ArrayList<Character>> offspringChromosomes = new ArrayList<ArrayList<Character>>(); 
+
+    //generate an individual's 8 character chromosome
+    Individual(int size){
+      chromosome = new ArrayList<Character>(size);
+      for (int i = 0; i < size; i++){
+        Character ch = randomLetter(4);
+        chromosome.add(ch);
+      }
+
+     // System.out.println(chromosome);
+    }
+    //generate initialize population of cergtani size, and each member of population has 8 chromosome letters
+    //misread instructions, this isnt' needed
+   /*  private Individual(int population){
+      for (int member = 0; member < population; member++){
+        memberChromosomes.add(generateChromosome(8));
+      }
+      System.out.println(memberChromosomes);
+    }*/
+
+    public int getFitness(){
+      ArrayList<Character> input = chromosome; 
+      int leftPointer = 0;
+      int rightPointer = input.size() -1;
+      int fitness = 0; //assuming fitness starts at 0;
+      if (input.size() % 2 != 0){
+        fitness ++;  //if chromosome size odd, then by default middle letter maps onto itself and fitness +1
+      }
+      while (leftPointer < rightPointer){
+        if (input.get(leftPointer) == input.get(rightPointer)){
+          fitness ++;
+        }
+        leftPointer ++;
+        rightPointer--;
+      }
+      return fitness;
+    }
+    //make offspring
+//Test
+    Individual(ArrayList<Individual> parent1, ArrayList<Individual> parent2, double mutationRate, int c_max){
+      int randomLength1 = ThreadLocalRandom.current().nextInt(1, parent1.toString().length());
+      int randomLength2 = ThreadLocalRandom.current().nextInt(1, parent2.toString().length());
+      System.out.println(randomLength1);
+      System.out.println(randomLength2);
+      for (int i = 1; i < randomLength1; i++){
+        Character ch = parent1.toString().charAt(i);
+        chromosome.add(ch);
+      }
+      for (int i = 1; i < randomLength2; i++){
+        chromosome.add(parent2.toString().charAt(i));
+      }
+      if (chromosome.size() > c_max){
+        chromosome = new ArrayList<Character>(chromosome.subList(0, c_max));
+      }
+      for (int i = 0; i < chromosome.size(); i ++){
+        if (ThreadLocalRandom.current().nextDouble() < mutationRate){
+          chromosome.set(i, randomLetter(4));
+        }
+      }
+      System.out.println(chromosome);
+    
+    }
+
+      /*for (int i = 0; i < randomLength1; i++){
+        Character ch = parent1.chromosome.get(i);
+        chromosome.add(ch);
+      }
+      for (int i = 0; i < randomLength2; i++){
+        chromosome.add(parent2.get(i));
+      }
+      if (chromosome.size() > c_max){
+        chromosome = new ArrayList<Character>(chromosome.subList(0, c_max));
+      }
+      for (int i = 0; i < chromosome.size(); i ++){
+        if (ThreadLocalRandom.current().nextDouble() < mutationRate){
+          chromosome.set(i, randomLetter(4));
+        }
+      }
+      System.out.println(chromosome);
+    }
+
+    private Individual(ArrayList<Character> parent1, ArrayList<Character> parent2, double mutationRate, int c_max){
+      int randomLength1 = ThreadLocalRandom.current().nextInt(1, parent1.size());
+      int randomLength2 = ThreadLocalRandom.current().nextInt(1, parent2.size());
+
+      for (int i = 0; i < randomLength1; i++){
+        Character ch = parent1.get(i);
+        chromosome.add(ch);
+      }
+      for (int i = 0; i < randomLength2; i++){
+        chromosome.add(parent2.get(i));
+      }
+      if (chromosome.size() > c_max){
+        chromosome = new ArrayList<Character>(chromosome.subList(0, c_max));
+      }
+      for (int i = 0; i < chromosome.size(); i ++){
+        if (ThreadLocalRandom.current().nextDouble() < mutationRate){
+          chromosome.set(i, randomLetter(4));
+        }
+      }
+      System.out.println(chromosome);
+    }*/
+
+     /** Expresses the individual's chromosome 
+     *  as a String, for display purposes */
+    public String toString() {
+      StringBuilder builder = new StringBuilder(chromosome.size());
+      for(Character ch: chromosome) {
+        builder.append(ch);
+      }
+      return builder.toString();
+      //return chromosome.stream().map(e->e.toString()).collect(Collectors.joining());
+    }
+
+    private Character randomLetter(int num_dna_letters) {
+        return Character.valueOf((char)(65+ThreadLocalRandom.current().nextInt(num_dna_letters)));
+    }
+
+    public static void main(String[] args){
+        //System.out.println(individual.generateChromosome(10)); generateChromosome looks normal
+       //System.out.println(individual.chromosome.toString()); chromosome  does display with toString, but you can also just print individual.chromosome
+       /*ArrayList<Character> parent1 = new ArrayList<Character>(
+            Arrays.asList('A',
+                          'B',
+                          'C',
+                          'A',
+                          'D',
+                          'B',
+                          'C',
+                          'A',
+                          'D',
+                          'B',
+                          'C',
+                          'A',
+                          'D',
+                          'B',
+                          'C',
+                          'A',
+                          'D',
+                          'B',
+                          'C',
+                          'A',
+                          'D',
+                          'B',
+                          'C',
+                          'A',
+                          'D',
+                          'B',
+                          'C',
+                          'A',
+                          'D',
+                          'B',
+                          'C',
+                          'A',
+                          'D',
+                          'B',
+                          'C',
+                          'A',
+                          'D'
+                          ));
+      ArrayList<Character> parent2 = new ArrayList<Character>(
+            Arrays.asList('D',
+                          'B',
+                          'B',
+                          'D',
+                          'C'));
+
+      Individual individual = new Individual(Individual parent1.chromsome, Individual parent2, 0.01, 20); //yep, stops at 20 
+      System.out.println(individual.chromosome.size()); //yep, always less than or equal to 20
+       //individual.Individual(individual.memberChromosomes.get(1), individual.memberChromosomes.get(2));
+      System.out.println(parent1);
+      System.out.println(individual.getFitness());
+
+      */
+      Individual indv1 = new Individual(8);
+      Individual indv2 = new Individual(8);
+      
+    }
+
+
+}
